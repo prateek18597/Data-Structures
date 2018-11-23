3bd2892e3263afd28e343ff2725be7e9a8a37c6a
Pratik Parmar <prateek18597@gmail.com>
2018-11-23 23:47:21 +0000

    Added Methods in Linked List

--- C++/LinkedList/LinkedList.cpp
+++ C++/LinkedList/LinkedList.cpp
@@ -2,54 +2,62 @@
 #include<string.h>
 using namespace std;
 
-template <class T>
-struct Node
+template <class T>//Using generics to use linked list with any data type.
+
+struct Node//Structure of Node of Linked List. 
 {
 	T value;
-	Node<T> *prev=NULL;
-	Node<T> *next=NULL;
+	//Add more variables here according to need.
+	Node<T> *prev=NULL;//Pointer to previous element in the list
+	Node<T> *next=NULL;//Pointer to next element in the list
 };
 
 template<class T>
-class LinkedList
+class LinkedList//Blueprint of linked list class.
 {
-	public:
+	private:
+	
 		Node<T> * head;
 		Node<T> * tail;
-		int length;
+		int len;
+	
+	public:
+	
 		LinkedList<T>()
 		{
 			head=NULL;
 			tail=NULL;
-			length=0;
+			len=0;
 		}
 
 		bool isEmpty();
-		int length1();
+		int length();
 		void display();
 		void append_front(T value);
 		void append_back(T value);
+		void delete_element(T v);//Will delete first occurence of element containing v as value
+		// Declare more methods here according to need
 };
 
 template<class T>
-bool LinkedList<T>::isEmpty()
+bool LinkedList<T>::isEmpty()//Check if list is empty or not
 {
-	return length==0;
+	return len==0;
 }
 
 template<class T>
-int LinkedList<T>::length1()
+int LinkedList<T>::length()//Returns length of linked list
 {
-	return length;
+	return len;
 }
 
 template<class T>
-void LinkedList<T>::append_front(T value)
+void LinkedList<T>::append_front(T value)//Append in head of linked list
 {
 	Node<T> *node=new Node<T>;
 	node->value=value;
 
-	if(length==0)
+	if(len==0)
 	{
 		head=node;
 		tail=node;
@@ -62,16 +70,16 @@ void LinkedList<T>::append_front(T value)
 		head=node;
 		node=NULL;
 	}
-	length++;
+	len++;
 }
 
 template<class T>
-void LinkedList<T>::append_back(T value)
+void LinkedList<T>::append_back(T value)//Append in tail of linked list
 {
 	Node<T> *node=new Node<T>;
 	node->value=value;
 
-	if(length==0)
+	if(len==0)
 	{
 		head=node;
 		tail=node;
@@ -84,11 +92,11 @@ void LinkedList<T>::append_back(T value)
 		tail=node;
 		node=NULL;
 	}
-	length++;
+	len++;
 }
 
 template<class T>
-void LinkedList<T>::display()
+void LinkedList<T>::display()//To display element in linked list
 {
 	Node<T> *node=new Node<T>;
 	node=head;
@@ -97,16 +105,52 @@ void LinkedList<T>::display()
 		cout<<"-->"<<node->value;
 		node=node->next;
 	}
-	cout<<"-->"<<node->value;
+	cout<<"-->"<<node->value<<endl;
+}
+
+template<class T>
+void LinkedList<T>::delete_element(T v)//Delete a node with value v from linked list(Only first occurence)
+{
+	Node<T> * temp=new Node<T>;
+	temp=head;
+	
+	while(temp->next!=NULL)
+	{
+		if(temp->value==v)
+		{
+			if(temp==head)
+			{
+				head=head->next;
+			}
+			else
+			{
+				temp->prev->next=temp->next;
+				temp->next->prev=temp->prev;
+			}
+			len--;
+			free(temp);
+			break;//Remove this break to remove only to first occurence from the front of linked list.
+		}
+		temp=temp->next;
+	}
+	if(temp->value==v)
+	{	
+		tail=temp->prev;
+		temp->prev->next=NULL;
+		len--;
+		free(temp);
+	}
 }
 
 int main()
 {
 	LinkedList<string> list;
-	list.append_back("Aaa");
-	list.append_back("Bbb");
-	list.append_back("Ccc");
-	list.append_back("Ddd");
+	list.append_front("Aaa");
+	list.append_front("Bbb");
+	list.append_front("Ccc");
+	list.append_front("Ddd");
+	list.display();
+	list.delete_element("Aaa");
 	list.display();
 	return 0;
 }
---
+++ C++/LinkedList/Readme.md
@@ -0,0 +1,20 @@
+#Bidirectional Linked List
+
+##Parameters in Node of Linked List:
+	*value*
+	*next*
+	*prev*
+
+##Methods in LinkedList class
+	isEmpty()
+		Returns if list is empty or not.
+	length()
+		Returns length of linked list.
+	append_front(<datatype> value)
+		Append element at the front of linked list.
+	append_back(<datatype> value)
+		Append element at the back of linked list.
+	display()
+		Displays element in linked list
+	delete_element(T value)
+		Deletes element with value v from linked list.
